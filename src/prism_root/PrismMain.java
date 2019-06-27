115347de0aa56f39c18c0bbab8389e01d22a7648
Dung Nguyen <dzung.csu@gmail.com>
2019-06-27 11:22:27 +0700

    Prism version 1.2.07
    Former-commit-id: d3f667ff874fe6141446ffd605320b54dcd1fcff
    Former-commit-id: 29cdf77c7b4b998ff75d29f8454b36bfa6fe8cda

--- src/prism_root/PrismMain.java
+++ src/prism_root/PrismMain.java
@@ -94,7 +94,7 @@ public class PrismMain extends JFrame {
 	private MenuItem_SetTransparency 	setTransparency;		// For menuWindow
 	private MenuItem_CaptureGUI 		captureGUI;				// For menuWindow
 		
-	private static String 				prism_version = "PRISM ALPHA 1.2.06";
+	private static String 				prism_version = "PRISM ALPHA 1.2.07";
 	private String 						currentProject;
 	private static PrismDesktopPane 	prism_DesktopPane;
 	private static PrismContentPane 	prism_ContentPane;
