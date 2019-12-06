3ddbd086ca3ee621ab1754c8baeb3f8a81e7aa43
Dung Nguyen <dzung.csu@gmail.com>
2019-12-06 22:03:55 +0700

    wrap up for merging
    Former-commit-id: 96011cd16670ac5256c7e5256ade2f49f74f2896
    Former-commit-id: e179dfbd0206b38e969dc4379b5ccd65fd2a59c5

--- Projects/new_project_02/1.3.02_2. PA_22_15/readme.txt
+++
@@ -1,2 +0,0 @@
-Model is last edited by:     PRISM ALPHA 1.2.08     on     2019/07/27   -   10:11:02
-Browse & Import a database before writting here
--- src/prism_root/PrismMain.java
+++ src/prism_root/PrismMain.java
@@ -94,7 +94,7 @@ public class PrismMain extends JFrame {
 	private MenuItem_SetTransparency 	setTransparency;		// For menuWindow
 	private MenuItem_CaptureGUI 		captureGUI;				// For menuWindow
 		
-	private static String 				prism_version = "PRISM ALPHA 2.0.01"; 
+	private static String 				prism_version = "PRISM ALPHA 2.0.02"; 
 	private String 						currentProject;
 	private static PrismDesktopPane 	prism_DesktopPane;
 	private static PrismContentPane 	prism_ContentPane;
